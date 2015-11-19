f99433afb46f2938b7b8815d1f9cb9a9510d3a0f
RobSmyth <rob@rbs.id.au>
2015-11-19 19:48:54 +1100

    Upload missing files

---
+++ Dashboard.Framework/Config/Parameters/HyperlinkElementViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+
+
+namespace NoeticTools.Dashboard.Framework.Config.Parameters
+{
+    public class HyperlinkElementViewModel : IElementViewModel
+    {
+        public HyperlinkElementViewModel(string text, ICommand command)
+        {
+            Parameters = new object[] {text, command};
+            ElementType = ElementType.Hyperlink;
+            Value = text;
+            Name = string.Empty;
+        }
+
+        public string Name { get; }
+        public ElementType ElementType { get; }
+        public object[] Parameters { get; }
+        public object Value { get; set; }
+    }
+}

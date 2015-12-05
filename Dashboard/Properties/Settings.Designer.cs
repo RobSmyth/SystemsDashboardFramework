d04de0b2d5fe9e6176a76ae61c96fddac803ced9
RobSmyth <rob@rbs.id.au>
2015-12-06 09:26:17 +1100

    Renaming project to Systems Dashboard Framework

--- Dashboard/Properties/Settings.Designer.cs
+++ Dashboard/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace NoeticTools.TeamDashboard.Properties {
+namespace NoeticTools.SystemsDashboard.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]

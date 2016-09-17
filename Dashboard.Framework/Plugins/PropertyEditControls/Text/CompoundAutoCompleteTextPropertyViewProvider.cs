63d06ac20139582d7c85563eff08ee63458348a7
RobSmyth <rob@rbs.id.au>
2016-09-17 16:38:10 +1000

    Adding compound properties

--- Dashboard.Framework/Plugins/PropertyEditControls/Text/CompoundAutoCompleteTextPropertyViewProvider.cs
+++ Dashboard.Framework/Plugins/PropertyEditControls/Text/CompoundAutoCompleteTextPropertyViewProvider.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Drawing;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,37 +24,46 @@ namespace NoeticTools.TeamStatusBoard.Framework.Plugins.PropertyEditControls.Tex
 
         public FrameworkElement Create(IPropertyViewModel viewModel, int rowIndex, string elementName)
         {
-            var upperPanel = new StackPanel() { HorizontalAlignment = HorizontalAlignment.Stretch, Orientation = Orientation.Vertical };
-            upperPanel.Children.Add(CreateComboBox(viewModel, "A"));
-            upperPanel.Children.Add(CreateComboBox(viewModel, "B"));
+            var valuesPanel = new StackPanel() { HorizontalAlignment = HorizontalAlignment.Stretch, Orientation = Orientation.Vertical };
+            var values = ((string) viewModel.Value).Split(',');
+            foreach (var value in values)
+            {
+                valuesPanel.Children.Add(CreateComboBox(viewModel, value, () => viewModel.Value = GetCompountValue(valuesPanel)));
+            }
 
             var panel = new StackPanel() { HorizontalAlignment = HorizontalAlignment.Stretch, Orientation = Orientation.Vertical };
-            panel.Children.Add(upperPanel);
-            panel.Children.Add(new Button() { Content = "Add", HorizontalAlignment = HorizontalAlignment.Right, Margin = new Thickness(1), Padding = new Thickness(4,1,4,1)});
+            panel.Children.Add(valuesPanel);
+            var addButton = new Button() { Content = "Add", HorizontalAlignment = HorizontalAlignment.Right, Margin = new Thickness(1), Padding = new Thickness(4,1,4,1)};
+            addButton.Click += (a,b) => valuesPanel.Children.Add(CreateComboBox(viewModel, "", () => viewModel.Value = GetCompountValue(valuesPanel)));
+            panel.Children.Add(addButton);
 
             return new Border() {BorderThickness = new Thickness(1), BorderBrush = System.Windows.Media.Brushes.Gray, Child = panel, Padding = new Thickness(1)};
         }
 
-        private static FrameworkElement CreateComboBox(IPropertyViewModel viewModel, string elementName)
+        private string GetCompountValue(Panel valuesPanel)
+        {
+            var values = valuesPanel.Children.Cast<ComboBox>().Select(x => x.Text).Where(y => !string.IsNullOrWhiteSpace(y));
+            return string.Join(",", values);
+        }
+
+        private static FrameworkElement CreateComboBox(IPropertyViewModel viewModel, string initialValue, Action onFocusLostAction)
         {
             var comboBox = new ComboBox
             {
                 HorizontalAlignment = HorizontalAlignment.Stretch,
                 FontSize = 12.0,
-                Name = elementName.Replace('.', '_'),
                 DataContext = viewModel,
                 IsEditable = true,
                 IsTextSearchEnabled = true,
                 IsTextSearchCaseSensitive = false,
                 StaysOpenOnEdit = true,
-                Text = (string) viewModel.Value,
+                Text = initialValue,
                 Margin = new Thickness(1)
             };
             comboBox.GotFocus += (a, b) => comboBox.IsDropDownOpen = false;
-            comboBox.LostFocus += (a, b) => viewModel.Value = comboBox.Text;
+            comboBox.LostFocus += (a, b) => onFocusLostAction();
 
             BindingOperations.SetBinding(comboBox, ComboBox.ItemsSourceProperty, new Binding("Parameters"));
-
 
             return comboBox;
         }
